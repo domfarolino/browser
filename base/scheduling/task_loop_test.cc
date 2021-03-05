4276794e09e403dd476a8dcc623b6d3e4b84a9ab
Dominic Farolino <domfarolino@gmail.com>
2021-03-05 13:32:29 -0700

    Clean

--- base/scheduling/task_loop_test.cc
+++ base/scheduling/task_loop_test.cc
@@ -1,26 +1,31 @@
 #include "gtest/gtest.h"
 
-#include <iostream>
-
 #include "base/scheduling/task_loop.h"
 #include "base/threading/simple_thread.h"
 
+// These tests are general TaskLoop tests that should pass for all kinds of task
+// loops regardless of |base::ThreadType|.
+
+namespace base {
+
 TEST(TaskLoopTest, QuitBeforeRun) {
-  auto task_loop = base::TaskLoop::Create(base::ThreadType::WORKER);
+  auto task_loop = TaskLoop::Create(ThreadType::WORKER);
   task_loop->Quit();
   task_loop->Run(); // Loop should immediately quit. Test should not time out.
 }
 
 TEST(TaskLoopTest, RunQuitRunQuit) {
-  auto task_loop = base::TaskLoop::Create(base::ThreadType::WORKER);
+  auto task_loop = TaskLoop::Create(ThreadType::WORKER);
 
-  base::SimpleThread t1([](base::Callback quit_closure) {
+  SimpleThread t1([](Callback quit_closure) {
     quit_closure();
   }, task_loop->QuitClosure());
   task_loop->Run();
 
-  base::SimpleThread t2([](base::Callback quit_closure) {
+  SimpleThread t2([](Callback quit_closure) {
     quit_closure();
   }, task_loop->QuitClosure());
   task_loop->Run();
 }
+
+}; // namespace base
