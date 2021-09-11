86abda26c96593699a03d683badba3d1029473af
Dominic Farolino <domfarolino@gmail.com>
2021-09-10 20:10:04 -0400

    Make the bind example

--- examples/bind.cc
+++ examples/bind.cc
@@ -2,10 +2,16 @@
 
 #include "base/callback.h"
 
+void FunctionA(int n) {
+  std::cout << "FunctionA called with n = " << n << std::endl;
+}
+
+void FunctionB(base::OnceClosure cb) {
+  cb();
+}
+
 int main() {
-  base::OnceClosure closure = [](){
-    std::cout << "OnceClosure() is being invoked()" << std::endl;
-  };
-  closure();
+  base::OnceClosure closure = base::BindOnce(FunctionA, 101);
+  FunctionB(std::move(closure));
   return 0;
 }
